2586ef15a178ad4b867c0e3ac9b59c15fa0d01da
Shehan <ertsshehan@gmail.com>
2025-06-16 11:31:31 +0530

    Refactor Boy class for setter-based dependency injection and Constructor injection
    Replaced direct object creation with setter-based dependency injection to improve flexibility and adherence to dependency injection principles. This change enables easier testing and better manageability of dependencies.

--- 03_DI_With_Spring/src/main/java/lk/ijse/edu/bean/Boy.java
+++ 03_DI_With_Spring/src/main/java/lk/ijse/edu/bean/Boy.java
@@ -2,9 +2,34 @@ package lk.ijse.edu.bean;
 
 public class Boy {
     //Property injection
-    Girl girl = new Girl();
+//    Girl girl = new Girl();
+//
+//    public void chatWithGirl() {
+//        girl.chat();
+//    }
+
+    //Constructor through dependency injection
+//    Girl girl;
+//
+//    public Boy(Girl girl) {
+//        this.girl = girl;
+//    }
+//
+//    public void chatWithGirl() {
+//        Boy boy = new Boy(new Girl());
+//        boy.chat();
+//    }
+
+    //Setter method through dependency injection
+    Girl girl;
+
+    public void setterMethod(Girl girl) {
+        this.girl = girl;
+    }
 
     public void chatWithGirl() {
-        girl.chat();
+        Boy boy = new Boy();
+        boy.setterMethod(new Girl());
+        boy.girl.chat();
     }
 }
