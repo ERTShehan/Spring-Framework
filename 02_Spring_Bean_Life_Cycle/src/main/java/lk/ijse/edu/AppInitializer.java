22185f1e2f3dfb2287c56c354f489db9d09af5cf
Shehan <ertsshehan@gmail.com>
2025-06-16 10:01:36 +0530

    Add MyConnection prototype bean to AppConfig
    Introduced a new `MyConnection` class and registered it as a prototype bean in `AppConfig`. Updated `AppInitializer` to utilize the new `MyConnection` bean while commenting out previous usage of `SpringBean`. Integrated proper bean lifecycle handling with `context.registerShutdownHook()`.

--- 02_Spring_Bean_Life_Cycle/src/main/java/lk/ijse/edu/AppInitializer.java
+++ 02_Spring_Bean_Life_Cycle/src/main/java/lk/ijse/edu/AppInitializer.java
@@ -1,5 +1,6 @@
 package lk.ijse.edu;
 
+import lk.ijse.edu.bean.MyConnection;
 import lk.ijse.edu.bean.SpringBean;
 import lk.ijse.edu.config.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -11,10 +12,17 @@ public class AppInitializer {
         context.register(AppConfig.class);
         context.refresh();
 
-        SpringBean bean1 = context.getBean(SpringBean.class);
-        System.out.println(bean1);
+//        SpringBean bean1 = context.getBean(SpringBean.class);
+//        System.out.println(bean1);
+//
+//        SpringBean bean2 = context.getBean(SpringBean.class);
+//        System.out.println(bean2);
 
-        SpringBean bean2 = context.getBean(SpringBean.class);
-        System.out.println(bean2);
+        MyConnection myConnection1 = context.getBean(MyConnection.class);
+        System.out.println(myConnection1);
+        MyConnection myConnection2 = context.getBean(MyConnection.class);
+        System.out.println(myConnection2);
+
+        context.registerShutdownHook();
     }
 }
