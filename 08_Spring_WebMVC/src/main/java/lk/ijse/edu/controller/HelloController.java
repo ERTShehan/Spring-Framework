59e84ca634617a266017259bbe732134d22474eb
Shehan <ertsshehan@gmail.com>
2025-06-24 04:33:01 +0530

    Update HelloController with multiple GET endpoints
    Revised the `/hello` mapping to `hello` and added two distinct GET endpoints (`/hello/one` and `/hello/two`). Each endpoint returns a unique response, enhancing API functionality.

--- 08_Spring_WebMVC/src/main/java/lk/ijse/edu/controller/HelloController.java
+++ 08_Spring_WebMVC/src/main/java/lk/ijse/edu/controller/HelloController.java
@@ -5,14 +5,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("hello")
 public class HelloController {
     public HelloController() {
         System.out.println("HelloController Constructor");
     }
 
-    @GetMapping
-    public String hello() {
-        return "Hello, World!";
+    @GetMapping("one")
+    public String hello1() {
+        return "Hello, World1!";
+    }
+
+    @GetMapping("two")
+    public String hello2() {
+        return "Hello, World2!";
     }
 }
