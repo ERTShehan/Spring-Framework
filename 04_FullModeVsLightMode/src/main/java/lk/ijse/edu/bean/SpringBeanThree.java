2dba05f486843c07e56f0343c37dc662768449cc
Shehan <ertsshehan@gmail.com>
2025-06-23 10:38:54 +0530

    Remove commented-out bean definitions in SpringBeanThree

--- 04_FullModeVsLightMode/src/main/java/lk/ijse/edu/bean/SpringBeanThree.java
+++ 04_FullModeVsLightMode/src/main/java/lk/ijse/edu/bean/SpringBeanThree.java
@@ -7,19 +7,19 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SpringBeanThree {
-    @Bean
-    public SpringBeanOne springBeanOne() {
-        //interBean dependency
-        //Light mode
-        SpringBeanTwo springBeanTwo1 =SpringBeanTwo();
-        SpringBeanTwo springBeanTwo2 =SpringBeanTwo();
-        System.out.println(springBeanTwo1);
-        System.out.println(springBeanTwo2);
-        return new SpringBeanOne();
-    }
-
-    @Bean
-    public SpringBeanTwo springBeanTwo() {
-        return new SpringBeanTwo();
-    }
+//    @Bean
+//    public SpringBeanOne springBeanOne() {
+//        interBean dependency
+//        Light mode
+//        SpringBeanTwo springBeanTwo1 =SpringBeanTwo();
+//        SpringBeanTwo springBeanTwo2 =SpringBeanTwo();
+//        System.out.println(springBeanTwo1);
+//        System.out.println(springBeanTwo2);
+//        return new SpringBeanOne();
+//    }
+//
+//    @Bean
+//    public SpringBeanTwo springBeanTwo() {
+//        return new SpringBeanTwo();
+//    }
 }
