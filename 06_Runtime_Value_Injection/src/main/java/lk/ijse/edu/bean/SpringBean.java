cd37a2bf027448443c4737b94045f47ddd541e65
Shehan <ertsshehan@gmail.com>
2025-06-23 13:43:56 +0530

    Refactor `SpringBean` to implement `InitializingBean`
    Updated the `SpringBean` class to implement the `InitializingBean` interface and override `afterPropertiesSet` for better initialization handling. Removed and commented out constructor-based dependency injections in favor of property-based injection.

--- 06_Runtime_Value_Injection/src/main/java/lk/ijse/edu/bean/SpringBean.java
+++ 06_Runtime_Value_Injection/src/main/java/lk/ijse/edu/bean/SpringBean.java
@@ -1,22 +1,35 @@
 package lk.ijse.edu.bean;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SpringBean {
-//    public SpringBean() {}
-    @Autowired(required = false)
-    public SpringBean(@Value("shehan") String name) {
-        System.out.println("SpringBean Constructor: " + name);
-//        System.out.println("SpringBean Constructor: " + number);
-//        System.out.println("SpringBean Constructor: " + b);
+public class SpringBean implements InitializingBean {
+    @Value("shehen")
+    private String name;
+
+    public SpringBean() {
+        System.out.println(name);
     }
 
-    @Autowired(required = false)
-    public SpringBean(@Value("udara") String name, @Value("2025") int number) {
-        System.out.println("SpringBean Constructor: " + name);
-        System.out.println("SpringBean Constructor: " + number);
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println(name);
     }
+
+//    public SpringBean() {}
+//    @Autowired(required = false)
+//    public SpringBean(@Value("shehan") String name) {
+//        System.out.println("SpringBean Constructor: " + name);
+////        System.out.println("SpringBean Constructor: " + number);
+////        System.out.println("SpringBean Constructor: " + b);
+//    }
+//
+//    @Autowired(required = false)
+//    public SpringBean(@Value("udara") String name, @Value("2025") int number) {
+//        System.out.println("SpringBean Constructor: " + name);
+//        System.out.println("SpringBean Constructor: " + number);
+//    }
 }
