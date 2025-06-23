997cc7d18968115329446785e32a388666fe7756
Shehan <ertsshehan@gmail.com>
2025-06-23 14:26:57 +0530

    Add property support and configure Spring Beans with values
    Integrated `@PropertySource` in `AppConfig` and annotated `SpringBean` as `@Component` to load properties from `application.properties`. Introduced new database-related fields in `SpringBean` and removed/disabled system environment variable processing in `AppInitializer`. Added `application.properties` file to manage externalized configurations.

--- 07_Spring_Env/src/main/java/lk/ijse/edu/AppInitializer.java
+++ 07_Spring_Env/src/main/java/lk/ijse/edu/AppInitializer.java
@@ -13,23 +13,23 @@ public class AppInitializer {
         context.register(AppConfig.class);
         context.refresh();
 
-        //access system environment variables
-        Map<String, String> getenv = System.getenv();
-        for (String key:getenv.keySet()) {
-            System.out.println(key + " : " + getenv.get(key));
-        }
-
-        System.out.println("=======================================");
-
-        //java environment variables
-        Properties properties = System.getProperties();
-        for (String key : properties.stringPropertyNames()) {
-            System.out.println(key + " : " + properties.get(key));
-        }
-        System.out.println("=======================================");
-
-        String osName = System.getProperty("os.name");
-        System.out.println("OS Name: " + osName);
+//        //access system environment variables
+//        Map<String, String> getenv = System.getenv();
+//        for (String key:getenv.keySet()) {
+//            System.out.println(key + " : " + getenv.get(key));
+//        }
+//
+//        System.out.println("=======================================");
+//
+//        //java environment variables
+//        Properties properties = System.getProperties();
+//        for (String key : properties.stringPropertyNames()) {
+//            System.out.println(key + " : " + properties.get(key));
+//        }
+//        System.out.println("=======================================");
+//
+//        String osName = System.getProperty("os.name");
+//        System.out.println("OS Name: " + osName);
 
         context.registerShutdownHook();
     }
