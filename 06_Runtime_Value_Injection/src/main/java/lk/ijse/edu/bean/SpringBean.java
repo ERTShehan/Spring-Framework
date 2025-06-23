eaa4b28d2808def899f3c934841d49b65958c976
Shehan <ertsshehan@gmail.com>
2025-06-23 13:29:31 +0530

    Refactor SpringBean constructor to accept additional parameters.
    Added new parameters `number` and `b` to the SpringBean constructor to enable more dynamic value injection. Updated constructor log statements to reflect these changes for improved clarity and debugging.

--- 06_Runtime_Value_Injection/src/main/java/lk/ijse/edu/bean/SpringBean.java
+++ 06_Runtime_Value_Injection/src/main/java/lk/ijse/edu/bean/SpringBean.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class SpringBean {
 //    public SpringBean() {}
-    public SpringBean(@Value("udara") String name) {
-        System.out.println("SpringBean Constructor");
+    public SpringBean(@Value("udara") String name, @Value("2025") int number, @Value("true") boolean b) {
+        System.out.println("SpringBean Constructor: " + name);
+        System.out.println("SpringBean Constructor: " + number);
+        System.out.println("SpringBean Constructor: " + b);
     }
 }
