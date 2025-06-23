97b7c9fb5a07c751ad9c2942d5c891ada5807421
Shehan <ertsshehan@gmail.com>
2025-06-23 13:24:24 +0530

    Add @Value annotation to SpringBean constructor
    The @Value annotation is added to inject a default value ("udara") into the constructor parameter. This enables runtime value injection and enhances the bean's configurability.

--- 06_Runtime_Value_Injection/src/main/java/lk/ijse/edu/bean/SpringBean.java
+++ 06_Runtime_Value_Injection/src/main/java/lk/ijse/edu/bean/SpringBean.java
@@ -1,11 +1,12 @@
 package lk.ijse.edu.bean;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SpringBean {
-    public SpringBean() {}
-    public SpringBean(String name) {
+//    public SpringBean() {}
+    public SpringBean(@Value("udara") String name) {
         System.out.println("SpringBean Constructor");
     }
 }
