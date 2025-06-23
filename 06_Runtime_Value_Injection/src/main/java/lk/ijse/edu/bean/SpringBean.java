7d12934a01014d2d8ce6ffcb3d7c0529d7bc29b3
Shehan <ertsshehan@gmail.com>
2025-06-23 13:41:47 +0530

    Refactor SpringBean constructors and update @Autowired usage.
    Simplified SpringBean constructors by modifying parameters and making @Autowired annotation optional. Commented out unused constructor arguments for clarity and maintainability. This improves flexibility when creating SpringBean instances.

--- 06_Runtime_Value_Injection/src/main/java/lk/ijse/edu/bean/SpringBean.java
+++ 06_Runtime_Value_Injection/src/main/java/lk/ijse/edu/bean/SpringBean.java
@@ -7,13 +7,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class SpringBean {
 //    public SpringBean() {}
-    public SpringBean(@Value("udara") String name, @Value("2025") int number, @Value("true") boolean b) {
+    @Autowired(required = false)
+    public SpringBean(@Value("shehan") String name) {
         System.out.println("SpringBean Constructor: " + name);
-        System.out.println("SpringBean Constructor: " + number);
-        System.out.println("SpringBean Constructor: " + b);
+//        System.out.println("SpringBean Constructor: " + number);
+//        System.out.println("SpringBean Constructor: " + b);
     }
 
-    @Autowired
+    @Autowired(required = false)
     public SpringBean(@Value("udara") String name, @Value("2025") int number) {
         System.out.println("SpringBean Constructor: " + name);
         System.out.println("SpringBean Constructor: " + number);
