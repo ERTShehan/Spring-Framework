b6d5b3980d33366fdb419d6f577c7be6d879ab8f
Shehan <ertsshehan@gmail.com>
2025-06-30 13:57:13 +0530

    Refactor CustomerController to support saving customers via query parameters

--- 09_RESTController/src/main/java/lk/ijse/edu/controller/CustomerController.java
+++ 09_RESTController/src/main/java/lk/ijse/edu/controller/CustomerController.java
@@ -2,10 +2,7 @@ package lk.ijse.edu.controller;
 
 import jakarta.servlet.annotation.MultipartConfig;
 import lk.ijse.edu.dto.CustomerDTO;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/customer")
@@ -15,12 +12,23 @@ public class CustomerController {
     //CID - Customer ID
     //CNAME - Customer Name
     //CAddress - Customer Address
-    @PostMapping
-    public String saveCustomer(@ModelAttribute CustomerDTO customerDTO) {
-        //x-www-form-urlencoded data widiyata awai
-        System.out.println("Customer ID: " + customerDTO.getCID());
-        System.out.println("Customer Name: " + customerDTO.getCName());
-        System.out.println("Customer Address: " + customerDTO.getCAddress());
-        return "Customer saved";
+//    @PostMapping
+//    public String saveCustomer(@ModelAttribute CustomerDTO customerDTO) {
+//        //x-www-form-urlencoded data widiyata awai
+//        System.out.println("Customer ID: " + customerDTO.getCID());
+//        System.out.println("Customer Name: " + customerDTO.getCName());
+//        System.out.println("Customer Address: " + customerDTO.getCAddress());
+//        return "Customer saved";
+//    }
+
+    //save customer - Query Params
+    @PostMapping(params = {"CID", "CName", "CAddress"})
+    public String saveCustomerQueryParams(@RequestParam("CID") String CID, @RequestParam("CName") String CName, @RequestParam("CAddress") String CAddress) {
+        System.out.println("Customer ID: " + CID);
+        System.out.println("Customer Name: " + CName);
+        System.out.println("Customer Address: " + CAddress);
+        return "Customer saved via Query Params";
     }
+
+
 }
