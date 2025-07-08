96b87333a7d15f8d775252962922f9157d62d0a4
Shehan <ertsshehan@gmail.com>
2025-07-08 22:32:27 +0530

    Add CrossOrigin annotation to JobController for CORS support

--- 11_Back_End/src/main/java/lk/ijse/edu/back_end/controller/JobController.java
+++ 11_Back_End/src/main/java/lk/ijse/edu/back_end/controller/JobController.java
@@ -10,6 +10,7 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/job")
 @RequiredArgsConstructor
+@CrossOrigin
 public class JobController {
     private final JobServiceImpl jobService;
 
