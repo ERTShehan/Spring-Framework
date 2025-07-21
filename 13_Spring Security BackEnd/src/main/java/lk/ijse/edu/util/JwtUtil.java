81486331cd198681c3f431d3e2fabe9784c6fc5d
Shehan <ertsshehan@gmail.com>
2025-07-21 11:55:30 +0530

    Add application configuration and JWT utility for user authentication

---
+++ 13_Spring Security BackEnd/src/main/java/lk/ijse/edu/util/JwtUtil.java
@@ -0,0 +1,36 @@
+package lk.ijse.edu.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil { // token generate karanna , expire time check , etc..
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()), SignatureAlgorithm.HS256).compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(secretKey.getBytes())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
