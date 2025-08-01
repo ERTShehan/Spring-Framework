0735e72c6b77fefacd792d919b4adbd80d5aa4a8
Shehan <ertsshehan@gmail.com>
2025-08-01 11:57:00 +0530

    Implement ProductService and ProductServiceImpl for product management functionality

--- 14_Spring_Testing/src/main/java/lk/ijse/edu/service/ProductService.java
+++ 14_Spring_Testing/src/main/java/lk/ijse/edu/service/ProductService.java
@@ -1,4 +1,13 @@
 package lk.ijse.edu.service;
 
+import lk.ijse.edu.entity.Product;
+
+import java.util.List;
+
 public interface ProductService {
+    List<Product> getAllProducts();
+    Product getProductById(Long id);
+    Product createProduct(Product product);
+    Product updateProduct(Product product);
+    void deleteProduct(Long id);
 }
